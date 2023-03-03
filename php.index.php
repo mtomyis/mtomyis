50dabc1a2c6da4b6d9becb28adca2d4d6c9dc5e3
mtomyis <mtomyis@gmail.com>
2023-03-03 16:35:52 +0700

    rename

---
+++ php.index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Muhammad Tomy Iskandar</title>
+    <meta name="robots" content="noindex, nofollow">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+    <header id="header" class="d-flex flex-column justify-content-center">
+      <nav id="navbar" class="navbar nav-menu">
+        <ul>
+          <li>
+            <a href="#hero" class="nav-link scrollto active">
+              <i class="bx bx-home"></i>
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#about" class="nav-link scrollto">
+              <i class="bx bx-user"></i>
+              <span>About</span>
+            </a>
+          </li>
+          <li>
+            <a href="#resume" class="nav-link scrollto">
+              <i class="bx bx-file-blank"></i>
+              <span>Resume</span>
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" class="nav-link scrollto">
+              <i class="bx bx-book-content"></i>
+              <span>Portfolio</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <section id="hero" class="d-flex flex-column justify-content-center">
+      <div class="container" data-aos="zoom-in" data-aos-delay="100">
+        <h1>M. Tomy Iskandar</h1>
+        <p>I'm <span class="typed" data-typed-items="Developer, IT Support, Designer"></span>
+        </p>
+      </div>
+    </section>
+    <main id="main">
+      <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>About</h2>
+            <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
+          </div>
+          <div class="row">
+            <div class="col-lg-4">
+              <img src="assets/img/profile-img.jpeg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-8 pt-4 pt-lg-0 content">
+              <h3>Full Stack Developer, IT Support &amp; Designer.</h3>
+              <p class="fst-italic"> Continuous learning is essential in the rapidly evolving tech industry, and I am dedicated to staying up-to-date with the latest trends and technologies.</p>
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Birthday:</strong>
+                      <span>July 1998</span>
+                    </li>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Github:</strong>
+                      <span><a href="https://www.github.com/mtomyis" target="blank">www.github.com/mtomyis</a></span>
+                    </li>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Phone:</strong>
+                      <span><a href="https://wa.me/6282227342900?text=Hi%20Tom!" target="blank">082227342900</a> </span>
+                    </li>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>City:</strong>
+                      <span>Banyuwangi, Jawa Timur</span>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Age:</strong>
+                      <span><?= (date("Y")-1998); ?></span>
+                    </li>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Education:</strong>
+                      <span>D3 Teknik Informatika</span>
+                    </li>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Email:</strong>
+                      <span>
+                        <a href = "mailto: mtomyis@gmail.com" target="blank">mtomyis@gmail.com</a>
+                      </span>
+                    </li>
+                    <li>
+                      <i class="bi bi-chevron-right"></i>
+                      <strong>Freelance:</strong>
+                      <span>Available</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <p> Have an interest in development web and android based. As a programmer, my passion lies in solving complex problems through creative coding solutions..</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="skills" class="skills section-bg">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Skills</h2>
+          </div>
+          <div class="row skills-content">
+            <div class="col-lg-6">
+              <div class="progress">
+                <span class="skill">Website <i class="val">85%</i>
+                </span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <div class="progress">
+                <span class="skill">Mysql <i class="val">80%</i>
+                </span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <div class="progress">
+                <span class="skill">Design<i class="val">60%</i>
+                </span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="progress">
+                <span class="skill">Android <i class="val">75%</i>
+                </span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <div class="progress">
+                <span class="skill">SQLite <i class="val">70%</i>
+                </span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <div class="progress">
+                <span class="skill">Problem Solving <i class="val">80%</i>
+                </span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="resume" class="resume">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Resume</h2>
+          </div>
+          <div class="row">
+            <div class="col-lg-6">
+              <h3 class="resume-title">Sumary</h3>
+              <div class="resume-item pb-0">
+                <h4>M. Tomy Iskandar</h4>
+                <p>
+                  <em>Innovative Development Apps with <?= (date("Y")-2020); ?>+ years of experience developing Android and web from initial concept to final, polished deliverable.</em>
+                </p>
+                <ul>
+                  <li>Banyuwangi, Jawa Timur, Indonesia</li>
+                  <li><a href="https://wa.me/6282227342900?text=Hi%20Tom!" target="blank">082227342900</a></li>
+                  <li>
+                    <a href = "mailto: mtomyis@gmail.com" target="blank">mtomyis@gmail.com</a>
+                  </li>
+                </ul>
+              </div>
+              <h3 class="resume-title">Education</h3>
+              <div class="resume-item">
+                <h4>D3 Teknik Informatika</h4>
+                <h5>2016 - 2019</h5>
+                <p>
+                  <em>Politeknik Negeri Banyuwangi</em>
+                </p>
+                <p>Politeknik Negeri Banyuwangi is a polytechnic state university located in Labanasem, Banyuwangi, East Java, Indonesia.</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <h3 class="resume-title">Professional Experience</h3>
+              <div class="resume-item">
+                <h4>PT. Mascitra Teknologi Informasi</h4>
+                <h5>May 2020 - <?= date("F Y") ?></h5>
+                <p>
+                  <em>Programmer</em>
+                </p>
+                <ul>
+                  <li>Develop internal and external applications needed in the company</li>
+                  <li>Handle application maintenance complaints</li>
+                </ul>
+              </div>
+              <div class="resume-item">
+                <h4>PT. Concept Design Architect</h4>
+                <h5>April 2020 - March 2022</h5>
+                <p>
+                  <em>Full Stack Developer & IT Support</em>
+                </p>
+                <ul>
+                  <li>Make sure the internet runs smoothly.</li>
+                  <li>Perform maintenance on IT devices</li>
+                  <li>Develop internal applications needed in the company</li>
+                  <li>Handle complaints from all company employees regarding IT devices</li>
+                </ul>
+              </div>
+              <div class="resume-item">
+                <h4>PT. Barata Indonesia</h4>
+                <h5>November 2019 - March 2020</h5>
+                <p>
+                  <em>Biro Sistem Manajement Informasi</em>
+                </p>
+                <ul>
+                  <li>Make sure the internet runs smoothly.</li>
+                  <li>Perform maintenance on IT devices</li>
+                  <li>Develop internal applications needed in the company</li>
+                  <li>Handle complaints from all company employees regarding IT devices</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="portfolio" class="portfolio section-bg">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Portfolio</h2>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+              <ul id="portfolio-flters">
+                <li data-filter="*" class="filter-active">All</li>
+                <li data-filter=".Android">Android</li>
+                <li data-filter=".Web">Web</li>
+              </ul>
+            </div>
+          </div>
+          <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+            <?php
+            include('data.php');
+
+            foreach($data as $value){ ?>
+            <div class="col-lg-4 col-md-6 portfolio-item <?= $value['category'] ?>">
+              <div class="portfolio-wrap">
+                <img src="assets/img/portfolio/<?= $value['image1'] ?>" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4><?= $value['title'] ?></h4>
+                  <p><?= $value['category'] ?></p>
+                  <div class="portfolio-links">
+                    <a href="assets/img/portfolio/<?= $value['image1'] ?>" data-gallery="portfolioGallery" class="portfolio-lightbox" title="<?= $value['title'] ?>">
+                      <i class="bx bx-plus"></i>
+                    </a>
+                    <a href="portfolio-details.php?title=<?= $value['title'] ?>" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details">
+                      <i class="bx bx-link"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <?php } ?>
+            
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer id="footer">
+      <div class="container">
+        <h3>Muhammad Tomy Iskandar</h3>
+        <div class="copyright"> &copy; Copyright <strong>
+            <span><?= date("Y") ?></span>
+          </strong>. All Rights Reserved </div>
+        <div class="credits"> Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </footer>
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+      <i class="bi bi-arrow-up-short"></i>
+    </a>
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script>
+      if (window.self == window.top) {
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-P7JSYB1CSP');
+      }
+    </script>
+  </body>
+</html>
