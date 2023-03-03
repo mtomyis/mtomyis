fc612a6dd3daaae7f6ce5bc40c276cb7fab837d1
mtomyis <mtomyis@gmail.com>
2023-03-03 16:25:23 +0700

    first commit

---
+++ data.php
@@ -0,0 +1,167 @@
+<?php
+
+$data=
+[
+	[
+		'title'     => 'Absensi',
+		'category'  => 'Web',
+		'detail'	=> 'An attendance application to easily monitor the attendance of participants at a seminar using qrcode',
+		'image1'    => 'absensi 1.png',
+		'image2'    => 'absensi 2.png',
+		'image3'    => 'absensi 3.png'
+	],
+	[
+		'title'     => 'SIDIA',
+		'category'  => 'Web',
+		'detail'	=> 'Inventory information system that makes it easy for users to organize inventory data using the FIFO system',
+		'image1'    => 'sidia 1.png',
+		'image2'    => 'sidia 2.png',
+		'image3'    => 'sidia 3.png'
+	],
+	[
+		'title'     => 'SIMBMD',
+		'category'  => 'Web',
+		'detail'	=> 'Information system that manages regional property',
+		'image1'    => 'simbmd 1.png',
+		'image2'    => 'simbmd 2.png',
+		'image3'    => 'simbmd 3.png'
+	],
+	[
+		'title'     => 'SIMBIG',
+		'category'  => 'Web',
+		'detail'	=> 'Information Sistem that manages property at the company PT Industri Gula Glenmore',
+		'image1'    => 'simbig 1.png',
+		'image2'    => 'simbig 2.png',
+		'image3'    => 'simbig 3.png'
+	],
+	[
+		'title'     => 'Guestbook',
+		'category'  => 'Web',
+		'detail'	=> 'Guestbook management application and utilization of WhatsApp broadcast for promotion at Java Turtle Homestay',
+		'image1'    => 'turtle 1.png',
+		'image2'    => 'turtle 2.png',
+		'image3'    => 'turtle 3.png'
+	],
+	[
+		'title'     => 'Wedding Invitation',
+		'category'  => 'Web',
+		'detail'	=> 'Online Wedding Invitation',
+		'image1'    => 'wedding online 1.png',
+		'image2'    => 'wedding online 2.png',
+		'image3'    => 'wedding online 3.png'
+	],
+	[
+		'title'     => 'Re-Claim',
+		'category'  => 'Android',
+		'detail'	=> 'An application to record disbursements or reimbursements and make a report',
+		'image1'    => 're-claim 1.png',
+		'image2'    => 're-claim 2.png',
+		'image3'    => 're-claim 3.png'
+	],
+	// [
+	// 	'title'     => 'KV / Wakoel',
+	// 	'category'  => 'Android',
+	// 	'detail'	=> 'POS system that integrates marketing, sales and inventory',
+	// 	'image1'    => 'kv 1.png',
+	// 	'image2'    => 'kv 2.png',
+	// 	'image3'    => 'kv 3.png'
+	// ],
+	[
+		'title'     => 'Terminalku',
+		'category'  => 'Web',
+		'detail'	=> 'Weekly work monitoring and finance on project terminal construction',
+		'image1'    => 'terminal 1.png',
+		'image2'    => 'terminal 2.png',
+		'image3'    => 'terminal 3.png'
+	],
+	[
+		'title'     => 'GISPIPA',
+		'category'  => 'Web',
+		'detail'	=> 'Geographic information system, displaying hippam pipeline distribution lines',
+		'image1'    => 'gispipa 1.png',
+		'image2'    => 'gispipa 2.png',
+		'image3'    => 'gispipa 3.png'
+	],
+	[
+		'title'     => 'SKA & SKT',
+		'category'  => 'Web',
+		'detail'	=> 'Certificate of Expertise and Skills data management system (SKA SKT)',
+		'image1'    => 'ska 1.png',
+		'image2'    => 'ska 2.png',
+		'image3'    => 'ska 3.png'
+	],
+	[
+		'title'     => 'SIMANIS',
+		'category'  => 'Android',
+		'detail'	=> 'Project Monitoring System, includes the preparation of weekly reports, and reports project finance',
+		'image1'    => 'simanis 1.png',
+		'image2'    => 'simanis 2.png',
+		'image3'    => 'simanis 3.png'
+	],
+	[
+		'title'     => 'Web Company Profile',
+		'category'  => 'Web',
+		'detail'	=> 'Company information system at PT Concept Design Architect',
+		'image1'    => 'konseparsitek 1.png',
+		'image2'    => 'konseparsitek 2.png',
+		'image3'    => 'konseparsitek 3.png'
+	],
+	[
+		'title'     => 'Letter Service Information System',
+		'category'  => 'Web',
+		'detail'	=> 'A letter-making service system to shorten the flow of letter-making at Bulusari Village',
+		'image1'    => 'pelayanan 1.png',
+		'image2'    => 'pelayanan 2.png',
+		'image3'    => 'pelayanan 3.png'
+	],
+	[
+		'title'     => 'Ping App',
+		'category'  => 'Android',
+		'detail'	=> 'Wifi router monitoring application at PT Barata Indonesia',
+		'image1'    => 'ping 1.png',
+		'image2'    => 'ping 2.png',
+		'image3'    => 'ping 3.png'
+	],
+	[
+		'title'     => 'Complaint Note App',
+		'category'  => 'Android',
+		'detail'	=> 'Application for recording complaints IT at PT Barata Indonesia',
+		'image1'    => 'pengaduan 1.png',
+		'image2'    => 'pengaduan 2.png',
+		'image3'    => 'pengaduan 3.png'
+	],
+	[
+		'title'     => 'Fish Feeder App',
+		'category'  => 'Android',
+		'detail'	=> 'Smart Fishpond Monitoring And Control System In Real Time Based On IoT With MQTT Protocol Data Transmission',
+		'image1'    => 'fish 1.png',
+		'image2'    => 'fish 2.png',
+		'image3'    => 'fish 3.png'
+	],
+	[
+		'title'     => 'Pandutani App',
+		'category'  => 'Android',
+		'detail'	=> 'The food farming guide application contains plant information, calculation of planting schedules, disease expert systems, as well as group discussions',
+		'image1'    => 'pandutani 1.png',
+		'image2'    => 'pandutani 2.png',
+		'image3'    => 'pandutani 3.png'
+	],
+	[
+		'title'     => 'Dikopi App',
+		'category'  => 'Android',
+		'detail'	=> 'Diagnostic application on coffee plants, to find out coffee diseases',
+		'image1'    => 'dikopi 1.png',
+		'image2'    => 'dikopi 2.png',
+		'image3'    => 'dikopi 3.png'
+	],
+	[
+		'title'     => 'Pixelstory App',
+		'category'  => 'Android',
+		'detail'	=> 'Image editing application',
+		'image1'    => 'pixelstory 1.png',
+		'image2'    => 'pixelstory 2.png',
+		'image3'    => 'pixelstory 3.png'
+	],
+];
+
+?>
